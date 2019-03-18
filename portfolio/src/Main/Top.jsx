192fa571e715c3d9aaaf9d04b265e7d733376863
TItanGuy99 <35585118+TItanGuy99@users.noreply.github.com>
2019-03-17 22:02:31 -0300

    Preparing the structure for the components

---
+++ portfolio/src/Main/Top.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Top extends Component{
+    render(){
+        return(
+            <div>
+                <h2>My portfolio</h2>
+            </div>
+        );
+    }
+}
+
+export default Top;
