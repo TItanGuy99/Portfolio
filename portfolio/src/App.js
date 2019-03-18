c4c4e4ee2d87b9832bf35aba3c9982a150a218ed
luiznai <luiznai@br.ibm.com>
2019-03-18 16:45:36 -0300

    Routers implemented.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Top from './Main/Top';
-import Info from './Main/Info'
-import Footer from './Main/Footer'
-import MenuBar from './Main/MenuBar'
+import Handler from './Main/Handler'
+
 
 class App extends Component {
   render() {
       return (
         <div className="App">
-            <Top />
-            <MenuBar />    
-            <Info />
-            <Footer email='luiz.neosnk@gmail.com' linkedin='https://www.linkedin.com/in/luiz-nai/' />
+          <Handler />
         </div>
       );
   }
---
+++ portfolio/src/Main/Education.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Education extends Component{
+
+
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-3"></div>
+                    <div className="col-6 padding-top">
+                        <p>
+                            Teste
+                        </p>
+                    </div>
+                    <div className="col-3"></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Education;
