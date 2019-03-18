b145126bdc07acf700c467406d32752d618f71a2
luiznai <luiznai@br.ibm.com>
2019-03-18 15:26:48 -0300

    Menu bar has been added.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Top from './Main/Top';
 import Info from './Main/Info'
 import Footer from './Main/Footer'
+import MenuBar from './Main/MenuBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Top />
+          
           <Info />
           <Footer email='luiz.neosnk@gmail.com' linkedin='https://www.linkedin.com/in/luiz-nai/' />
       </div>
--- portfolio/src/Main/MenuBar.jsx
+++ portfolio/src/Main/MenuBar.jsx
@@ -1,4 +1,4 @@
-import React, { Components } from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -10,7 +10,22 @@ class MenuBar extends Component{
     render(){
         return(
             <div>
-                
+                <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+                </nav>
             </div>
         )
     }
