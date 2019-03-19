6caa90fc1a3f304fba42ab37225a4190f75b502b
luiznai <luiznai@br.ibm.com>
2019-03-19 13:51:39 -0300

    Images fix.

--- portfolio/src/Main/SideProjects.jsx
+++ portfolio/src/Main/SideProjects.jsx
@@ -7,9 +7,16 @@ class SideProjects extends Component{
         return(
             <div>
                <div className="row">
+
+               <div className="col-2"></div>
+                    <div className="col-8">
+                    <img className="rounded float-left image-size padding-top" src="/images/titan.jpg"></img>
+                    </div>
                     <div className="col-2"></div>
-                    <div className="col-8 padding-top">
-                        <p className="info-alignment-left">
+
+                    <div className="col-2"></div>
+                    <div className="col-8">
+                        <p className="info-alignment-left padding-top">
                             <strong>Titan Game Studios</strong> (2016 - Present)
                         </p>
                         <p className="info-alignment-left">
@@ -18,9 +25,6 @@ class SideProjects extends Component{
                         <p className="info-alignment-left">
                             Titan Game Studios is my side project. Here I develop tools and games for consoles and mobile.
                         </p>
-                        <ul className="info-alignment-left">
-                           <li>C, C++, C#, ActionScript, HTML5, JavaScript and CSS.</li>
-                        </ul>
                         <p className="info-alignment-left">
                             <a target="_blank" href="http://www.titangamestudios.com">http://www.titangamestudios.com</a>
                         </p>
@@ -32,8 +36,17 @@ class SideProjects extends Component{
                     <div className="col-2"></div>
 
                     <div className="col-2"></div>
-                    <div className="col-8 padding-top">
-                        <p className="info-alignment-left">
+                    <div className="col-8">
+                    <img className="rounded float-left image-size" src="/images/cpm.jpeg"></img>
+                    </div>
+                    <div className="col-2"></div>
+
+                    
+                    
+
+                    <div className="col-2"></div>
+                    <div className="col-8">
+                        <p className="info-alignment-left padding-top">
                             <strong>Cidadão Pró-Mundo</strong> (2018 - Present)
                         </p>
                         <p className="info-alignment-left">
@@ -49,30 +62,7 @@ class SideProjects extends Component{
                     <div className="col-2"></div>
 
 
-                    <div className="col-2"></div>
-
-          
-
-
-                        
-                    <div className="col-8 card mb-3">
-                        <img className="card-img-top" src="/images/cpm.jpeg"></img>
-                        <div className="card-body">
-                            <h5 className="card-title info-alignment-left"><strong>Cidadão Pró-Mundo</strong> (2018 - Present)</h5>
-                            <p className="card-text info-alignment-left">Teaching English language for people with limited access to education.</p>
-                            <p className="info-alignment-left">
-                            <a target="_blank" href="https://www.cidadaopromundo.org/">https://www.cidadaopromundo.org/</a>
-                        </p>
-                        </div>
-                    </div>
-
-        
-
-           
-
             
-                    <div className="col-2"></div>
-
 
                 </div>
             </div>
