5f0714a74eaf777aa485621d3e72462992c6548a
TItanGuy99 <35585118+TItanGuy99@users.noreply.github.com>
2019-03-20 23:59:36 -0300

    final_html

---
+++ portfolio/docs/precache-manifest.5caf0cd2c89466e7fd4cfcaa2891f4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "356dee45c192ed080f8a",
+    "url": "/static/js/main.294bcaf4.chunk.js"
+  },
+  {
+    "revision": "ab1c7139db77830c8689",
+    "url": "/static/js/2.34848491.chunk.js"
+  },
+  {
+    "revision": "356dee45c192ed080f8a",
+    "url": "/static/css/main.fd2296dc.chunk.css"
+  },
+  {
+    "revision": "ab1c7139db77830c8689",
+    "url": "/static/css/2.38abfe6c.chunk.css"
+  },
+  {
+    "revision": "4c9669ba42555cd20c7c114ca0b35906",
+    "url": "/index.html"
+  }
+];
