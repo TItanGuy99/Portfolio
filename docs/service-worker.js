dd1fb7f175fb094dca14aef5a31f9b19310902f2
TItanGuy99 <35585118+TItanGuy99@users.noreply.github.com>
2019-03-21 00:33:11 -0300

    Test github.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1e8895097e8675a0c96f9eca0740eff.js"
+  "/luiznai-portfolio.github.io/precache-manifest.eff410ff5c80b50a97f0abefcad60a9e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/luiznai-portfolio.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
